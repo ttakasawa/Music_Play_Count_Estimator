d6198704fbce689cc198e205775c4846d4aca7d0
Kyle Salitrik <nullfragment@gmail.com>
2018-03-16 00:31:36 -0400

    Replaced selectFeatureTerminal with driver code

--- FeatureExtraction/selectFeatureTerminal.py
+++ FeatureExtraction/selectFeatureTerminal.py
@@ -1,36 +1,33 @@
-from __future__ import print_function
-import librosa
-from pydub import AudioSegment
 import os
-#from audioSegmentation.py import function(a,b)
-from extractBeat import getBeat
-from audioSegmentation import split
+import csv
+import re
+import FeatureExtraction.audioSegmentation as Segment
 
-AddressOfInputAudio = ""
-NameOfSong = ""
+song_title_index = 0
+media_dir = input("Enter the music directory:")
+data_folder = input("Enter the destination directory:")
 
-#######################    splitting audio    ################################
-# get estimated tempo, beat per minute
-#	-> param: Name of audio w/o .mp3, isDynamic
-#										-> Static: 0, dynamic: 1
-#
-dest = ""
-split(AddressOfInputAudio, NameOfSong, dest)
-#
-###############################################################################
+# Creates destination outer folder if nonexistent
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
 
+# Read in selected songs into list and remove header
+with open('../DatasetSelection/selected_songs.csv', 'r') as file:
+    reader = csv.reader(file)
+    selected_songs = list(reader)
+selected_songs.pop(0)
 
-###########################    getTempo    ####################################
-# get estimated tempo, beat per minute
-#	-> param: Name of audio w/o .mp3, isDynamic
-#										-> Static: 0, dynamic: 1
-#audioName = 'a2'
-#print(getBeat(audioName, 1))
-#
-###############################################################################
+# Create split audio files
+for song in selected_songs:
+    # Either regex will work, but split is possibly faster
+    # name = re.findall("[0-9]*[^\.mp3]", song[song_title_index])
+    name = re.split("\.", song[song_title_index])[0]
+    in_file = os.path.join(media_dir, song[song_title_index])
 
+    # Create song directories, load audio, and calculate number of segments
+    audio_data, num_segments, song_path, segment_path = Segment.AudioSegmentationPrep(in_file, name, data_folder)
 
-
-
-
-
+    # Iterate over number of segments
+    for current_segment in range(0, num_segments):
+        audio_segment_data = Segment.GetNextSegment(audio_data, current_segment)
+        Segment.SaveAudio(name, audio_segment_data, current_segment, segment_path)
