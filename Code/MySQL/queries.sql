1ae3b7f81132e7c376433793b5096a5db75acad3
Kyle Salitrik <nullfragment@gmail.com>
2018-04-10 16:39:39 -0400

    Updated queries to find top 5 users
    Users are found in the following fashion:
    
    From the original top users, count the number plays for each song by all users.
    
    From those songs, get all songs with 50 or more total plays.
    
    Using those songs, get the total playcounts for each user in the top users table.
    
    Pick the top 5 users from this query and then select all play records for these 5 users for all of the top aggregated songs.
    
    All 278 songs that were listened to by these users with a total of 50 or more plays between all users were exported.
    
    Ignoring all the minimum total plays yields 872 songs.

--- Code/MySQL/queries.sql
+++ Code/MySQL/queries.sql
@@ -5,7 +5,7 @@ USE DS340_Project;
 ###############################################################
 SELECT user_id, song_id, plays
 FROM PlayRecords
-WHERE plays > 201
+WHERE plays < 201
 ORDER BY song_id;
 
 ###############################################################
@@ -59,7 +59,6 @@ FROM UniqueTracks,
 WHERE UniqueTracks.song_id = unique_songs.song_id;
 
 
-
 SELECT UniqueTracks.artist_name, UniqueTracks.song_title
 FROM UniqueTracks,
   (SELECT DISTINCT TopUserPlayRecords.song_id
@@ -67,5 +66,41 @@ FROM UniqueTracks,
 WHERE UniqueTracks.song_id = unique_songs.song_id
 INTO OUTFILE '/home/kyle/git/UnsupervisedDeepMusicRecommendation/MySQL/Results/artist_song_pairs.csv'
 FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+  ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
+
+###############################################################
+## Narrow Down Songs Chosen
+###############################################################
+
+# SELECT SONGS LISTENED TO BY 50 OR MORE USERS
+CREATE VIEW aggregate_song_counts AS
+SELECT song_id, COUNT(user_id) AS total_users
+FROM TopUserPlayRecords
+GROUP BY song_id
+HAVING total_users >= 50
+ORDER BY total_users DESC;
+
+# Get top users with highest total playcounts for these songs
+CREATE VIEW selected_users AS
+SELECT TopUserPlayRecords.user_id, SUM(TopUserPlayRecords.plays) total_user_playcounts
+FROM TopUserPlayRecords, aggregate_song_counts
+WHERE aggregate_song_counts.song_id = TopUserPlayRecords.song_id
+GROUP BY TopUserPlayRecords.user_id
+HAVING total_user_playcounts > 1800
+ORDER BY total_user_playcounts DESC;
+
+# GET UNIQUE RECORDS FOR THESE SONG, USER PAIRS
+SELECT PlayRecords.user_id, PlayRecords.song_id, PlayRecords.plays
+FROM PlayRecords, aggregate_song_counts, selected_users
+WHERE selected_users.user_id = PlayRecords.user_id
+      AND PlayRecords.song_id = aggregate_song_counts.song_id;
+
+# GET LIST OF ALL SONGS LISTENED TO BY TOP 5 USERS WITH A TOTAL OF 50+ PLAYS AMONGST ALL USERS
+SELECT DISTINCT Top5_User_Records.song_id
+FROM  Top5_User_Records;
+
+# GET LIST OF ALL SONGS LISTENED TO BY TOP 5 USERS IGNORING MINIMUM PLAYS
+SELECT DISTINCT PlayRecords.song_id
+FROM PlayRecords, selected_users
+WHERE selected_users.user_id = PlayRecords.user_id;
