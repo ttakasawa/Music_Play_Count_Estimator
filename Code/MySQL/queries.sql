15476f40a7fd0ab782ac1a9eac0c947db137550a
Kyle Salitrik <nullfragment@gmail.com>
2018-04-15 15:41:29 -0400

    Corrected significant issues in SQL queries

--- Code/MySQL/queries.sql
+++ Code/MySQL/queries.sql
@@ -1,106 +1,67 @@
 USE DS340_Project;
 
 ###############################################################
-## USER PLAY THRESHOLD
+## THRESHOLD MAX PLAYS PER RECORD
 ###############################################################
-SELECT user_id, song_id, plays
-FROM PlayRecords
-WHERE plays < 201
-ORDER BY song_id;
+# Create a view of all play records with 200 or less plays
+DROP VIEW truncated_records;
+CREATE VIEW truncated_records AS
+  SELECT user_id, song_id, plays
+  FROM PlayRecords
+  WHERE plays < 201
+  ORDER BY song_id;
 
 ###############################################################
-## USER PLAY COUNTS
+## NARROW DOWN TO TOP 5 USERS BASED ON PLAY COUNTS
 ###############################################################
 
 ## Get total play counts from each user
-SELECT user_id, SUM(plays) AS total_user_playcount
-FROM PlayRecords
-WHERE plays < 201
-GROUP BY user_id
-ORDER BY total_user_playcount DESC;
+DROP VIEW user_play_counts;
+CREATE VIEW user_play_counts AS
+  SELECT user_id, SUM(plays) AS total_user_playcount
+  FROM truncated_records
+  GROUP BY user_id
+  ORDER BY total_user_playcount DESC;
 
-#### IMPORTED NEW TABLE AS RESULT OF ABOVE QUERY
+#### CREATED NEW TABLE FROM ABOVE VIEW
 
-## Check Number of Users given Play Threshold
-SELECT UserPlaycounts.user_id, UserPlaycounts.total_user_playcount
-FROM UserPlaycounts
-WHERE UserPlaycounts.total_user_playcount > 1612
-      AND UserPlaycounts.total_user_playcount < 6000
-ORDER BY UserPlaycounts.total_user_playcount DESC;
+## Get list of users given a minimum number of plays
+DROP VIEW users_min_plays;
+CREATE VIEW users_min_plays AS
+  SELECT UserPlaycounts.user_id, UserPlaycounts.total_user_playcount
+  FROM UserPlaycounts
+  WHERE UserPlaycounts.total_user_playcount > 1612
+  ORDER BY UserPlaycounts.total_user_playcount DESC;
 
-## Get all songs listened to from above users
-SELECT PlayRecords.user_id, PlayRecords.song_id, PlayRecords.plays
-FROM PlayRecords
-  INNER JOIN (SELECT UserPlaycounts.user_id, UserPlaycounts.total_user_playcount
-              FROM UserPlaycounts
-              WHERE UserPlaycounts.total_user_playcount > 1612
-                    AND UserPlaycounts.total_user_playcount < 6000) AS TopUsers
-    ON TopUsers.user_id = PlayRecords.user_id
-WHERE plays < 201
-ORDER BY plays DESC;
-
-#### IMPORTED NEW TABLE AS RESULT OF ABOVE QUERY
-
-
-###############################################################
-## USER PLAY RECORD AGGREGATES
-###############################################################
-SELECT DISTINCT song_id
-FROM TopUserPlayRecords;
-# 101823 songs
-
-SELECT DISTINCT user_id
-FROM TopUserPlayRecords;
-
-SELECT UniqueTracks.artist_name, UniqueTracks.song_title
-FROM UniqueTracks,
-  (SELECT DISTINCT TopUserPlayRecords.song_id
-   FROM TopUserPlayRecords) unique_songs
-WHERE UniqueTracks.song_id = unique_songs.song_id;
-
-
-SELECT UniqueTracks.artist_name, UniqueTracks.song_title
-FROM UniqueTracks,
-  (SELECT DISTINCT TopUserPlayRecords.song_id
-   FROM TopUserPlayRecords) unique_songs
-WHERE UniqueTracks.song_id = unique_songs.song_id
-INTO OUTFILE '/home/kyle/git/UnsupervisedDeepMusicRecommendation/MySQL/Results/artist_song_pairs.csv'
-FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
+# Get top 5 users by number of plays
+DROP VIEW top_5_users;
+CREATE VIEW top_5_users AS
+  SELECT UserPlaycounts.user_id, UserPlaycounts.total_user_playcount
+  FROM UserPlaycounts
+  ORDER BY UserPlaycounts.total_user_playcount DESC
+  LIMIT 5;
 
 ###############################################################
 ## Narrow Down Songs Chosen
 ###############################################################
 
-# SELECT SONGS LISTENED TO BY 50 OR MORE USERS
-CREATE VIEW aggregate_song_counts AS
-SELECT song_id, COUNT(user_id) AS total_users
-FROM TopUserPlayRecords
-GROUP BY song_id
-HAVING total_users >= 50
-ORDER BY total_users DESC;
+# Get list of songs listened to by the top 5 users
+DROP VIEW top_user_play_records;
+CREATE VIEW top_user_play_records AS
+  SELECT truncated_records.user_id, truncated_records.song_id, truncated_records.plays
+  FROM truncated_records, top_5_users
+  WHERE top_5_users.user_id = truncated_records.user_id
+  ORDER BY plays DESC;
 
-# Get top users with highest total playcounts for these songs
-CREATE VIEW selected_users AS
-SELECT TopUserPlayRecords.user_id, SUM(TopUserPlayRecords.plays) total_user_playcounts
-FROM TopUserPlayRecords, aggregate_song_counts
-WHERE aggregate_song_counts.song_id = TopUserPlayRecords.song_id
-GROUP BY TopUserPlayRecords.user_id
-HAVING total_user_playcounts > 1800
-ORDER BY total_user_playcounts DESC;
+# Get total play counts for songs listened to by top 5 users.
+DROP VIEW top_song_plays;
+CREATE VIEW top_song_plays AS
+  SELECT song_id, SUM(plays) AS total_song_plays
+  FROM top_user_play_records, top_5_users
+  WHERE top_user_play_records.user_id = top_5_users.user_id
+  GROUP BY song_id
+  ORDER BY total_song_plays DESC;
 
-# GET UNIQUE RECORDS FOR THESE SONG, USER PAIRS
-SELECT PlayRecords.user_id, PlayRecords.song_id, PlayRecords.plays
-FROM PlayRecords, aggregate_song_counts, selected_users
-WHERE selected_users.user_id = PlayRecords.user_id
-      AND PlayRecords.song_id = aggregate_song_counts.song_id;
-
-# GET LIST OF ALL SONGS LISTENED TO BY TOP 5 USERS WITH A TOTAL OF 50+ PLAYS AMONGST ALL USERS
-SELECT DISTINCT Top5_User_Records.song_id
-FROM  Top5_User_Records;
-
-# GET LIST OF ALL SONGS LISTENED TO BY TOP 5 USERS IGNORING MINIMUM PLAYS
-SELECT DISTINCT PlayRecords.song_id
-FROM PlayRecords, selected_users
-WHERE selected_users.user_id = PlayRecords.user_id;
+# Get unique list of songs from top played songs
+SELECT DISTINCT song_id
+FROM top_song_plays;
