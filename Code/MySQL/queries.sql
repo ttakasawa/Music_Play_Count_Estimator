c10f2970f6d368294f087f3abc5c43b14c72fc15
Kyle Salitrik <nullfragment@gmail.com>
2018-04-15 15:59:17 -0400

    Added queries to get user play informaton for top 5 users

--- Code/MySQL/queries.sql
+++ Code/MySQL/queries.sql
@@ -51,7 +51,7 @@ CREATE VIEW top_user_play_records AS
   SELECT truncated_records.user_id, truncated_records.song_id, truncated_records.plays
   FROM truncated_records, top_5_users
   WHERE top_5_users.user_id = truncated_records.user_id
-  ORDER BY plays DESC;
+  ORDER BY top_5_users.user_id;
 
 # Get total play counts for songs listened to by top 5 users.
 DROP VIEW top_song_plays;
@@ -62,6 +62,44 @@ CREATE VIEW top_song_plays AS
   GROUP BY song_id
   ORDER BY total_song_plays DESC;
 
+###############################################################
+## QUERIES FOR CSV EXPORT
+###############################################################
+
 # Get unique list of songs from top played songs
 SELECT DISTINCT song_id
 FROM top_song_plays;
+
+# Get top 5 users
+SELECT *
+FROM top_5_users;
+
+# Create unique report for each of the top 5 users to easily
+# import data to python
+
+# SELECT user_id, song_id, plays / max_plays.max AS normalized_plays
+# FROM top_user_play_records,
+#   (SELECT MAX(plays) AS max
+#    FROM top_user_play_records
+#    WHERE user_id = '119b7c88d58d0c6eb051365c103da5caf817bea6') AS max_plays
+# WHERE user_id = '119b7c88d58d0c6eb051365c103da5caf817bea6';
+
+SELECT *
+FROM top_user_play_records
+WHERE user_id = '119b7c88d58d0c6eb051365c103da5caf817bea6';
+
+SELECT *
+FROM top_user_play_records
+WHERE user_id = '3fa44653315697f42410a30cb766a4eb102080bb';
+
+SELECT *
+FROM top_user_play_records
+WHERE user_id = 'a2679496cd0af9779a92a13ff7c6af5c81ea8c7b';
+
+SELECT *
+FROM top_user_play_records
+WHERE user_id = 'd7d2d888ae04d16e994d6964214a1de81392ee04';
+
+SELECT *
+FROM top_user_play_records
+WHERE user_id = '4ae01afa8f2430ea0704d502bc7b57fb52164882';
